3f5a249f4bbd6ca969626c0bcbb2c951c802ce49
howaiyung <howaiyung1988@gmail.com>
2019-01-31 22:30:52 -0700

    Basic screen with two inputs
    Currently fiddling around with the UI and basic entries. Finally figured out how I can not commit. A .gitignore file needs to be created in the root of the project.

---
+++ randoGenNum/randoGenNum/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace randoGenNum
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnGenerateButtonClicked(object sender, EventArgs e)
+        {
+            var num1 = firstNumEntry;
+            var num2 = secondNumEntry;
+
+
+        }
+    }
+}
