d2d1c473116cea0093255badcb16dea017606618
howaiyung <howaiyung1988@gmail.com>
2019-01-31 22:49:24 -0700

    Added the basic code logic
    Added the basic code logic to the random number generator.
    
    This youtube helped me clarify how to display error messages:
    https://www.youtube.com/watch?v=xg9gY6VUBAs
    
    This website also showed me how to get git working for a Xamarin project:
    https://stackoverflow.com/questions/47460039/vs-2017-git-local-commit-db-lock-error-on-every-commit/47472811

--- randoGenNum/randoGenNum/MainPage.xaml.cs
+++ randoGenNum/randoGenNum/MainPage.xaml.cs
@@ -16,10 +16,45 @@ namespace randoGenNum
 
         private void OnGenerateButtonClicked(object sender, EventArgs e)
         {
-            var num1 = firstNumEntry;
-            var num2 = secondNumEntry;
+            var num1 = firstNumEntry.Text;
+            var num2 = secondNumEntry.Text;
 
+            if(ProveInt(num1) == true)
+            {
+                ErrorLabelNum1.Text = "This is a valid integer";
+            }
+            else
+            {
+                ErrorLabelNum1.Text = "This is a invalid integer";
+            }
 
+            if (ProveInt(num2) == true)
+            {
+                ErrorLabelNum2.Text = "This is a valid integer";
+            }
+            else
+            {
+                ErrorLabelNum2.Text = "This is a invalid integer";
+            }
+        }
+
+        private int[] GenerateNumbers(int num1, int num2)
+        {
+            int[] randNumArray = new int[num1];
+
+            return randNumArray;
+        }
+
+        private bool ProveInt(String num)
+        {
+            int nVal = 0;
+
+            if(int.TryParse(num, out nVal))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
